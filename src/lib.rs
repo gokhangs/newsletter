fbc320d93ac558e843089ce35bac95fb29c1ae9d
Gokhan Sagirlar <gsagirlar@qpq.io>
2022-10-26 21:54:32 +0300

    feat (WIP): subscription work start

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-use actix_web::{dev::Server, get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, get, web::{self, Form}, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
+use serde;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+async fn subscribe(_form : web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
