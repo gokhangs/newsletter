07b2eecb8e3d3f4ddd2bc2a4243e6b69c7b6fe5d
Gokhan Sagirlar <gsagirlar@qpq.io>
2022-10-26 20:57:30 +0300

    feat: OS assigned port, test passes

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, dev::Server};
+use actix_web::{dev::Server, get, web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind(("127.0.0.1", 8000))?
-    .run();
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
 
-    Ok(server)  
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use std::net::TcpListener;
+
 use actix_web;
 use zero2prod::run;
 
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    println!("{}", port);
+    run(listener)?.await
 }
